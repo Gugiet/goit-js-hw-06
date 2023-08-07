20e4bcf08a67629f27832e2eec374d9cee641396
Mariusz Guzowski <108026217+Gugiet@users.noreply.github.com>
2023-08-07 14:10:08 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
+const spanEl = document.querySelector("span#text");
+spanEl.style.fontSize = "56px";
 
 function changeFontSize() {
   const fontSizeValue = fontSizeControl.value;
